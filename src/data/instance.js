18be8846226ce8c722ba0b7dd729f1bf68cf40ea
katia alhaj <katiaalhaj54@gmail.com>
2026-01-11 14:52:59 +0300

    edit in api

--- src/data/instance.js
+++ src/data/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import i18next from "i18next";
 
 const instance = axios.create({
-    baseURL: "https://inibiq.hostorr.net/api",
+    baseURL: "https://www.inibiq.iq/api",
 });
 
 instance.interceptors.request.use(function(config) {
